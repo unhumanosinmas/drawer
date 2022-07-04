02b716f58e834c8a458e919fecb0879bde82e44e
Diego Enrique Hernández Cholotío <104159636+unhumanosinmas@users.noreply.github.com>
2022-07-04 16:33:37 -0600

    First Commit
    creación del proyecto

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Hola Mundo</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
